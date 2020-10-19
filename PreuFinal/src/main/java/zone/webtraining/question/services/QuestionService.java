9e0c12af66f20916a534d9b40ec8b5c736c39a5d
JulioBed3 <jubedoyag@unal.edu.co>
2020-10-18 21:03:05 -0500

    bucle preguntas genericas

--- PreuFinal/src/main/java/zone/webtraining/question/services/QuestionService.java
+++ PreuFinal/src/main/java/zone/webtraining/question/services/QuestionService.java
@@ -3,10 +3,17 @@ import zone.webtraining.question.Lista;
 import zone.webtraining.question.models.Question;
 
 public class QuestionService extends Lista {
+
     public Lista getAll(){
-        Question prueba= new Question(1,"Funciona?","si","a medias","un poquito","casi nada","nada","si");
-        Lista preguntas=new Lista();
-        preguntas.insertar(prueba);
+
+        int num = 10000;
+        Lista preguntas = new Lista();
+        Question prueba;
+        for(int i = 0; i < num; i++){
+            prueba = new Question(i+1, "Pregunta"+(i+1), "Opcion 1", "Opcion 2", "Opcion 3", "Opcion 3", "Opcion 4", "Opcion 5");
+            preguntas.insertar(prueba);
+        }
+
         return preguntas;
     }
 }
