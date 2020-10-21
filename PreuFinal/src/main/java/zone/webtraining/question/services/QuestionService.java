8cc01dd41607e32261fe44f84afa542f6fdf52ab
JulioBed3 <jubedoyag@unal.edu.co>
2020-10-20 19:07:36 -0500

    clases ordenadas, clases nuevas: main, gui

--- PreuFinal/src/main/java/zone/webtraining/question/services/QuestionService.java
+++ PreuFinal/src/main/java/zone/webtraining/question/services/QuestionService.java
@@ -6,6 +6,8 @@ public class QuestionService extends Lista {
 
     public Lista getAll(){
 
+        long start = System.nanotime();
+
         int num = 10000;
         Lista preguntas = new Lista();
         Question prueba;
@@ -13,6 +15,11 @@ public class QuestionService extends Lista {
             prueba = new Question(i+1, "Pregunta"+(i+1), "Opcion 1", "Opcion 2", "Opcion 3", "Opcion 3", "Opcion 4", "Opcion 5");
             preguntas.insertar(prueba);
         }
+
+        long end = System.nanotime();
+
+        double time = (end - start) * 1.0e-9;
+        System.out.println();
 
         return preguntas;
     }
